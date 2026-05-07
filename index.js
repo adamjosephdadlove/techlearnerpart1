3592d36cbe84d443576fc877cb30132920752c6e
adamjosephdadlove <adamjosephdadlove@gmail.com>
2026-05-06 20:03:42 -0600

    Refactor menu toggle and resize event listener

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 const menuBtn = document.getElementById('menu-btn');
 const navBar = document.getElementById('nav-bar');
 
-// Toggle Menu
+
 menuBtn.addEventListener('click', () => {
   navBar.classList.toggle('active');
 });
 
-// Clean up state on resize
-window.addEventListener('resize', () => {
+window.addEventListener('resize', function(){
   if (window.innerWidth > 768) {
     navBar.classList.remove('active');
   }
